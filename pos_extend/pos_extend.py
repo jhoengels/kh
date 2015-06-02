fb50535b0228a0a9662f38f4a883b6524f3719c2
Javier huber Salazar Carlos <javier@salazarcarlos.com>
2015-06-02 15:34:54 -0500

    Añadiendo cambios

--- pos_extend/pos_extend.py
+++ pos_extend/pos_extend.py
@@ -32,6 +32,7 @@ from openerp.osv import osv,fields
 from openerp import netsvc
 from openerp.tools.translate import _
 import time
+import openerp.addons.decimal_precision as dp
 
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
@@ -55,6 +56,27 @@ class sale_shop(osv.osv):
 
 class pos_session(osv.osv):
     _inherit = 'pos.session'
+
+    def _confirm_orders(self, cr, uid, ids, context=None):
+        wf_service = netsvc.LocalService("workflow")
+
+        for session in self.browse(cr, uid, ids, context=context):
+            local_context = dict(context or {}, force_company=session.config_id.journal_id.company_id.id)
+            order_ids = [order.id for order in session.order_ids if order.state == 'paid']
+
+            move_id = self.pool.get('account.move').create(cr, uid, {'ref' : session.name, 'journal_id' : session.config_id.journal_id.id, }, context=local_context)
+
+            self.pool.get('pos.order')._create_account_move_line(cr, uid, order_ids, session, move_id, context=local_context)
+
+            for order in session.order_ids:
+                if order.state not in ('paid', 'invoiced','cancel'):
+                    raise osv.except_osv(
+                        _('Error!'),
+                        _("You cannot confirm all orders of this session, because they have not the 'paid' status"))
+                else:
+                    wf_service.trg_validate(uid, 'pos.order', order.id, 'done', cr)
+
+        return True
 
     def open_frontend_cb(self, cr, uid, ids, context=None):
         if not context:
@@ -83,6 +105,38 @@ class pos_session(osv.osv):
 class pos_order(osv.osv):
     _name = 'pos.order'
     _inherit ='pos.order'
+
+
+
+    def _amount_subtotal(self, cr, uid, ids, name, args, context=None):
+        tax_obj = self.pool.get('account.tax')
+        cur_obj = self.pool.get('res.currency')
+        res = {}
+        for order in self.browse(cr, uid, ids, context=context):
+            val1 = val2 = 0.0
+            cur = order.pricelist_id.currency_id
+            for line in order.lines:
+                #_logger.error("SHOP ID 0: %r", line.price_subtotal)
+                val1 += line.price_subtotal_incl
+                val2 += line.price_subtotal
+            res[order.id] = cur_obj.round(cr, uid, cur, val2)
+            
+        return res    
+
+    _columns = {
+        'es_factura': fields.boolean('Facturada', states={'draft': [('readonly', False)],} ),
+        'almacendespacho_id' : fields.many2one('stock.location', 'Almacen depacho', help="Ubicacion desde donde se despachara el producto al cliente, dejar en blanco si sale de la misma tienda", states={'draft': [('readonly', False)],} ),
+        'doc_number': fields.char('RUC/DNI',size=32, states={'draft': [('readonly', False)],} ),
+        #'devolucion': fields.char('Devolucion de',size=64, states={'draft': [('readonly', False)],} ),
+        'fecha_expected': fields.datetime('Fecha entrega al cliente', required=False, readonly=False, select=True, states={'draft': [('readonly', False)]}),
+        'partner_shipping_id': fields.many2one('res.partner', 'Direccion de entrega', states={'draft': [('readonly', False)],} ),
+
+        'amount_subtotal': fields.function(_amount_subtotal, digits_compute=dp.get_precision('Point Of Sale'), string='Sub Total',method=True, type='float',store=True),
+    }
+
+    _defaults = {
+        'es_factura': False,
+    }
 
     def write(self, cr, uid, ids, vals, context=None):
         res = super(pos_order, self).write(cr, uid, ids, vals, context=context)
@@ -113,37 +167,39 @@ class pos_order(osv.osv):
             else:
                 c.update({'location': posorder.shop_id.warehouse_id.lot_stock_id.id })
                 
-        #Fin
-        #_logger.error("PINCKING 1: %r", c)
+        #Fin        
         for order in self.browse(cr, uid, ids, context=c):
             for line in order.lines:
                 if order.state == 'draft':
                     if order.almacendespacho_id:
-                        if line.product_id.procedencia == 'import' :
+                        continue
+                        #if line.product_id.procedencia == 'import' :
+                        #if line.product_id.qty_disponible < line.qty:
+                        #    raise osv.except_osv(_('Warning!'),_('No puede vender un producto importado de almacen PRINCIPAL con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(line.product_id.name, line.product_id.qty_disponible,))
+                    else:
+                        if not order.picking_id:  
                             if line.product_id.qty_disponible < line.qty:
-                                #_logger.error("PINCKING 1: %r", line.product_id.procedencia)
-                                raise osv.except_osv(_('Warning!'),_('No puede vender un producto importado de almacen PRINCIPAL con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(line.product_id.name, line.product_id.qty_disponible,))
-                    else:  
-                        _logger.error("PINCKING 1: %r", line.product_id.qty_disponible)
-                        if line.product_id.qty_disponible < line.qty:
-                            raise osv.except_osv(_('Warning!'),_('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(line.product_id.name, line.product_id.qty_disponible,))
+                                raise osv.except_osv(_('Warning!'),_('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(line.product_id.name, line.product_id.qty_disponible,))
         #Fin     
-
         return res
 
-    _columns = {
-        'es_factura': fields.boolean('Facturada', states={'draft': [('readonly', False)],} ),
-        'almacendespacho_id' : fields.many2one('stock.location', 'Almacen depacho', help="Ubicacion desde donde se despachara el producto al cliente, dejar en blanco si sale de la misma tienda", states={'draft': [('readonly', False)],} ),
-        'doc_number': fields.char('RUC/DNI',size=32, states={'draft': [('readonly', False)],} ),
-        #'devolucion': fields.char('Devolucion de',size=64, states={'draft': [('readonly', False)],} ),
-        'fecha_expected': fields.datetime('Fecha entrega al cliente', required=False, readonly=False, select=True, states={'draft': [('readonly', False)]}),
-        'partner_shipping_id': fields.many2one('res.partner', 'Direccion de entrega', states={'draft': [('readonly', False)],} ),
-    }
+    def default_get(self, cr, uid, fields, context=None):
+        if context is None: context = {}
+        res = super(pos_order, self).default_get(cr, uid, fields, context=context)
+        order_ids = context.get('active_ids', [])
+        active_model = context.get('active_model')
 
-    _defaults = {
-        'es_factura': False,
-    }
+        so = self.pool.get('pos.session')
+        session_ids = so.search(cr, uid, [('state','=', 'opened')], context=context)
 
+        uo = self.pool.get('res.users')
+        user_id = uo.search(cr, uid, [('id','=', uid)], context=context)
+
+        for session in so.browse(cr, uid, session_ids, context=context):
+            for user in uo.browse(cr, uid, user_id, context=context):
+                if session.config_id.shop_id.id == user.shop_id.id:
+                    res.update(session_id=session.id )
+        return res   
 
     def onchange_partner_id(self, cr, uid, ids, part=False, context=None):
         if not part:
@@ -155,7 +211,8 @@ class pos_order(osv.osv):
         pricelist = self.pool.get('res.partner').browse(cr, uid, part, context=context).property_product_pricelist.id
         doc_number = self.pool.get('res.partner').browse(cr, uid, part, context=context).doc_number
         if doc_number:
-            if len(doc_number) == 11:
+            doc = doc_number.strip()
+            if len(doc) == 11:
                 return {'value': {'pricelist_id': pricelist, 'partner_shipping_id': addr['delivery'], 'doc_number':doc_number, 'es_factura': True}}
         
         return {'value': {'pricelist_id': pricelist, 'partner_shipping_id': addr['delivery'], 'doc_number':doc_number,'es_factura': False}}
@@ -370,26 +427,52 @@ class pos_order(osv.osv):
             if order.session_id.state == "closed":
                 raise osv.except_osv(_('Error!'), _('No se puede eliminar un ticket de una caja cerrada , debe de generar una note de credito'))
             if order.session_id.state != "closed":
-                self.cancel_payment(cr, uid, ids, context=context)
-                if order.almacendespacho_id:
-                    uid=1
-                wf_service.trg_validate(uid, 'stock.picking', order.picking_id.id, 'button_cancel', cr)
+                if order.statement_ids:
+                    raise osv.except_osv(_('ANULAR PAGO!'), _('No se puede eliminar un ticket PAGADO, Debe de anular el pago pirmero!'))
+                #self.cancel_payment(cr, uid, ids, context=context)                
+                if order.picking_id:
+                    if order.almacendespacho_id:
+                        uid=1
+                    wf_service.trg_validate(uid, 'stock.picking', order.picking_id.id, 'button_cancel', cr)
             #fin
-            if stock_picking_obj.browse(cr, uid, order.picking_id.id, context=context).state <> 'cancel':
-                raise osv.except_osv(_('Error!'), _('Unable to cancel the picking.'))
-        self.write(cr, uid, ids, {'state': 'cancel'}, context=context)
+            if order.picking_id:
+                if stock_picking_obj.browse(cr, uid, order.picking_id.id, context=context).state <> 'cancel':
+                    raise osv.except_osv(_('Error!'), _('Unable to cancel the picking.'))
+
+        self.write(cr, uid, ids, {'state': 'cancel', 'amount_total': 0.0}, context=context)
         return True
 
 pos_order()
 
 class pos_order_line(osv.osv):
-    _name = "pos.order.line"
-    _inherit = 'pos.order.line'
-    _columns = {
-        
+    _inherit = 'pos.order.line'  
+
+    def _amount_line_all(self, cr, uid, ids, field_names, arg, context=None):
+        res = dict([(i, {}) for i in ids])
+        account_tax_obj = self.pool.get('account.tax')
+        cur_obj = self.pool.get('res.currency')
+        for line in self.browse(cr, uid, ids, context=context):
+            taxes_ids = [ tax for tax in line.product_id.taxes_id if tax.company_id.id == line.order_id.company_id.id ]
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = account_tax_obj.compute_all(cr, uid, taxes_ids, price, line.qty, product=line.product_id, partner=line.order_id.partner_id or False)
+
+            cur = line.order_id.pricelist_id.currency_id
+            res[line.id]['price_subtotal'] = cur_obj.round(cr, uid, cur, taxes['total'])
+            res[line.id]['price_subtotal_incl'] = cur_obj.round(cr, uid, cur, taxes['total_included'])
+            #_logger.error("SHOP ID 1: %r", res)
+        return res
+    _columns = {#Añadimos precision para poder generar los asientos correctamente
+        'price_subtotal': fields.function(_amount_line_all, multi='pos_order_line_amount', string='Subtotal w/o Tax',digits_compute=dp.get_precision('Point Of Sale'), ),
+        'price_subtotal_incl': fields.function(_amount_line_all, multi='pos_order_line_amount', string='Subtotal',digits_compute=dp.get_precision('Point Of Sale'), ),
+  
     }
-    def onchange_product_id(self, cr, uid, ids, pricelist, product_id, qty=0, partner_id=False, almacendespacho=False, session_id=False, context=None):
+    def onchange_product_id(self, cr, uid, ids, pricelist, product_id, qty=0, partner_id=False, almacendespacho=False, session_id=False, context=None):        
         context = context or {}
+        if not  session_id:
+            raise osv.except_osv(_('Error de sesion!'), _('Debe de elegir una,\n sesion antes de ingrsar los productos a vender.'))
+                    
+        warning = {}      
+        warning_msgs = ''
 
         #Permite enviar el id del almacen de despacho
         c = context.copy()
@@ -397,7 +480,6 @@ class pos_order_line(osv.osv):
             c.update({'location': almacendespacho })
         else:
             session = self.pool.get('pos.session').browse(cr, uid, session_id) 
-            #_logger.error("LOOOOOOOOOOOO: %r", session.config_id.shop_id.warehouse_id.lot_stock_id.id)
             c.update({'location': session.config_id.shop_id.warehouse_id.lot_stock_id.id })            
         #Fin
 
@@ -408,35 +490,52 @@ class pos_order_line(osv.osv):
                'Please set one before choosing a product.'))
 
         #PERMITE VER SI LA CANTIDAD A VENDER ES PERMITIDA PARA PRODUCTOS IMPORTADOS
-        prod_id = self.pool.get('product.product').browse(cr, uid, product_id, context=c) 
-        #_logger.error("PINCK_CONTEXT1: %r", c)         
+        prod_id = self.pool.get('product.product').browse(cr, uid, product_id, context=c)        
         if almacendespacho:
-            if prod_id.procedencia == 'import':
-                if prod_id.qty_disponible < qty:
-                    raise osv.except_osv(_('Warning!'),_('No puede vender un producto importado del almacen PRINCIPAL con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(prod_id.name, prod_id.qty_disponible,))
+            #if prod_id.procedencia == 'import':
+            if prod_id.qty_disponible < qty:
+                warn_msg = _(('No puede vender un producto importado del almacen PRINCIPAL con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).')  %(prod_id.name, prod_id.qty_disponible,))
+                warning_msgs += _("Stock insuficiente ! : ") + warn_msg +"\n\n"
         else:
             if prod_id.qty_disponible < qty:
-                raise osv.except_osv(_('Warning!'),_('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(prod_id.name, prod_id.qty_disponible,))                
+                warn_msg = _(('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(prod_id.name, prod_id.qty_disponible,))
+                warning_msgs += _("Stock insuficiente ! : ") + warn_msg +"\n\n"
         #FIN        
 
         price = self.pool.get('product.pricelist').price_get(cr, uid, [pricelist],product_id, qty or 1.0, partner_id)[pricelist]
-        result = self.onchange_qty(cr, uid, ids, product_id, 0.0, qty, price, context=c)
-        result['value']['price_unit'] = price
-        return result
+        result = self.onchange_qty(cr, uid, ids, product_id, 0.0, qty, price, almacendespacho, session_id, context=c)
+        result['price_unit'] = price
+
+        if warning_msgs:
+            warning = {
+                       'title': ('Error de STOCK!'),
+                       'message' : warning_msgs
+                    }        
+
+        return {'value': result,'warning': warning}
 
 
-    def onchange_qty(self, cr, uid, ids, product, discount, qty, price_unit, almacendespacho=False, context=None):
+    def onchange_qty(self, cr, uid, ids, product, discount, qty, price_unit, almacendespacho=False, session_id=False, context=None):
+        context = context or {}
+        if not  session_id:
+            raise osv.except_osv(_('Error de sesion!'), _('Debe de elegir una,\n sesion antes de ingrsar los productos a vender.'))
+
         result = {}
-       
+        warning = {}
+        warning_msgs = ''
         
         #Permite enviar el id del almacen de despacho
         c = context.copy()
-        '''
-        almacendesp =None
+        
+        #almacendesp =None
         if almacendespacho:
             c.update({'location': almacendespacho })
-            almacendesp =  almacendespacho
-        '''
+            #almacendesp =  almacendespacho
+        else:
+            if session_id:
+                session = self.pool.get('pos.session').browse(cr, uid, session_id)
+                #_logger.error("PINCK_CONTEXT22222: %r", session_id) 
+                c.update({'location': session.config_id.shop_id.warehouse_id.lot_stock_id.id })
         #Fin
 
         if not product:
@@ -448,23 +547,33 @@ class pos_order_line(osv.osv):
 
         price = price_unit * (1 - (discount or 0.0) / 100.0)
         taxes = account_tax_obj.compute_all(cr, uid, prod.taxes_id, price, qty, product=prod, partner=False)
+        
 
         result['price_subtotal'] = taxes['total']
         result['price_subtotal_incl'] = taxes['total_included']
 
-        #PERMITE VER SI LA CANTIDAD A VENDER ES PERMITIDA PARA PRODUCTOS IMPORTADOS
-        '''      
-        _logger.error("PINCK_CONTEXT1: %r", almacendesp)        
-        if almacendesp:
-            if prod.procedencia == 'import':
-                if prod.qty_disponible < qty:
-                    raise osv.except_osv(_('Warning!'),_('Esta intentando vender un producto importado del almacen PRINCIPAL en una cantidad mayor a la cant. disponible, Producto: "%s" (Cant. Dispo:%d).') %(prod.name, prod.qty_disponible,))        
+        #PERMITE VER SI LA CANTIDAD A VENDER ES PERMITIDA 
+             
+        #_logger.error("PINCK_CONTEXT1: %r", almacendesp)        
+        if almacendespacho:
+            #if prod.procedencia == 'import':
+            if prod.qty_disponible < qty:
+                warn_msg = _(('No puede vender un producto importado del almacen PRINCIPAL con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).')  %(prod.name, prod.qty_disponible,))
+                warning_msgs += _("Stock insuficiente ! : ") + warn_msg +"\n\n"
         else:
             if prod.qty_disponible < qty:
-                raise osv.except_osv(_('Warning!'),_('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(prod.name, prod.qty_disponible,))
-        '''    
+                warn_msg = _(('No puede vender un producto con stock insuficiente, Producto: "%s" (Cant. Dispo:%d).') %(prod.name, prod.qty_disponible,))
+                warning_msgs += _("Stock insuficiente ! : ") + warn_msg +"\n\n"
+            
         #FIN
 
-        return {'value': result}
+        if warning_msgs:
+            warning = {
+                       'title': ('Error de STOCK!'),
+                       'message' : warning_msgs
+                    }
+
+
+        return {'value': result, 'warning': warning}
 
 
